e4a3b14d2c575aea4d9d22a0a36be5d55a2e3f3b
Noah Over <nko2@duke.edu>
2016-11-13 18:47:09 -0500

    created MockEnemy for example code

---
+++ src/authoring_interfaces/MockEnemy.java
@@ -0,0 +1,25 @@
+package authoring_interfaces;
+
+import javafx.geometry.Point2D;
+
+public class MockEnemy implements IEnemy {
+
+	@Override
+	public double getSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Point2D getSpawnPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point2D getEndPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
