dbf7f3127fff5e010acf9c8942b37b4470f9cdca
Alan Guo <ajg51@duke.edu>
2016-11-13 20:18:22 -0500

    Added code to some interfaces.

---
+++ src/engine/model/playerinfo/IViewablePlayerInfo.java
@@ -0,0 +1,6 @@
+package engine.model.playerinfo;
+
+public interface IViewablePlayerInfo {
+	public int getLivesRemaining();
+	public int getAvailableFunds();
+}
--- src/engine/model/playerinfo/gamestrategy/IEndCondition.java
+++ src/engine/model/playerinfo/gamestrategy/IEndCondition.java
@@ -1,5 +1,5 @@
 package engine.model.playerinfo.gamestrategy;
 
 public interface IEndCondition {
-
+	public boolean isConditionMet();
 }
