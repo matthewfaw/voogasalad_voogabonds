36c71f7b9c7bd09a859e2680895e421549e3a3f4
Weston Carvalho <wac12@duke.edu>
2016-11-16 15:02:20 -0500

    Changed Damage strategy arguments

--- src/engine/model/strategies/IDamageStrategy.java
+++ src/engine/model/strategies/IDamageStrategy.java
@@ -1,11 +1,12 @@
 package engine.model.strategies;
 
+import engine.model.machine.weapon.projectile.Projectile;
 import utility.Damage;
 import utility.Point;
 
 public interface IDamageStrategy {
 
-	Damage getAoEDamage(Point myLocation, double myHeading, Point location);
+	Damage getAoEDamage(Projectile missile, Point location);
 
 	Damage getTargetDamage();
 
