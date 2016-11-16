2edde684fe00d36786eee8d91d5831b2a9206439
Alan Guo <ajg51@duke.edu>
2016-11-16 17:52:10 -0500

    Add sell price getter to tower.

--- src/engine/model/machine/tower/Tower.java
+++ src/engine/model/machine/tower/Tower.java
@@ -5,8 +5,13 @@ import engine.model.machine.Machine;
 public class Tower extends Machine{
 	
 	private int upgradeCost;
+	private int sellPrice;
 	
 	public int getUpgradeCost() {
 		return upgradeCost;
+	}
+	
+	public int getSellPrice() {
+		return sellPrice;
 	}
 }
