f2e6a2b22b74fb8db54a65552a0d3fbce854fcd3
Tripp Whaley <dgw11@duke.edu>
2016-11-16 00:46:52 -0500

    beginning work on serialization

---
+++ src/authoring/model/serialization/SerializeJSON.java
@@ -0,0 +1,15 @@
+package authoring.model.serialization;
+
+
+import com.google.gson.*;
+
+public class SerializeJSON {
+	
+	private Gson gson;
+	
+	public void Serialize(){
+
+		gson = new Gson();
+			
+	}
+}
