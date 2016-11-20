a0b6ebc6cd4fb4cfecd7a9ce29c5241a8d0c3104
Philip Foo <pf50@duke.edu>
2016-11-19 22:36:45 -0500

    fixed small bug in playerdatacontroller

--- src/authoring/controller/WaveDataController.java
+++ src/authoring/controller/WaveDataController.java
@@ -1,9 +1,17 @@
 package authoring.controller;
 import java.util.AbstractMap;
 import authoring.model.WaveData;
+import authoring.view.objects.FrontEndWave;
 
 public class WaveDataController {
 	private AbstractMap<String, WaveData> myWaveMap;
 	
-	
+	public void createNewWave(FrontEndWave wave){
+		try{
+			//Create WaveData object here
+		}
+		catch(Exception e){
+			//Call front-end error handler;
+		}
+	}
 }
