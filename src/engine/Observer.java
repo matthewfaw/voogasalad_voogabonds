e2d8dc97286c9234a493dfd2944b7bd39a5b2d99
Weston Carvalho <wac12@duke.edu>
2016-11-20 19:25:50 -0500

    Clarifies Observer function.

--- src/engine/Observer.java
+++ src/engine/Observer.java
@@ -5,6 +5,19 @@ package engine;
 //We should talk about this
 //The issue is that this forces the observable to know exactly what kind of observer it's observing
 //which seems to be violating the data hiding that the observer/observable pattern provides
+
+/*
+ * The way I've been using this is that projectiles are constructed with an instance of Observer<IMovable>.
+ * (which would be a reference to some class in the View/Controller, probably the one that updates what objects are in the scene)
+ * After construction, the projectile calls myObserver.add(this) so that the scene knows to create an image to
+ * represent the new projectile. Every time the projectile moves, it calls myObserver.update(this) and it calls 
+ * myObserver.remove(this) when it explodes. It doesn't force the observer to know exactly what kind of thing it's
+ * observing, only that it implements whatever interface A is (IMovable in this case).
+ * 
+ * You can use Observer<IEntity> to do exactly the same thing you are doing with IObserver, except you get
+ * more control with the distinction between add, update, and remove.
+ */
+
 /**
  * This Interface allows an object to be notified of changes to objects of a specific type, given the implementing object
  * registers as an observer of the object of type A. 
@@ -14,7 +27,23 @@ package engine;
  */
 @Deprecated
 public interface Observer<A> {
+	
+	/**
+	 * aObjectToAdd calls add when it is created so the observer knows of it's existence.
+	 * @param aObjectToAdd
+	 */
 	abstract public void add(A aObjectToAdd);
+	
+	/**
+	 * aObjectToAdd calls update when its internal state has changed in a way that the observer needs to know about.
+	 * (Change in heading, position, or imagePath, etc.)
+	 * @param aObjectToUpdate
+	 */
 	abstract public void update(A aObjectToUpdate);
+	
+	/**
+	 * aObjectToAdd calls add when it is destroyed so the observer knows to stop representing/displaying it.
+	 * @param aObjectToRemove
+	 */
 	abstract public void remove(A aObjectToRemove);
 }
