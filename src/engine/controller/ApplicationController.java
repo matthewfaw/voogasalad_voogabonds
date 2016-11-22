d475682dbff5f34fccacc57cc10533f0bc21a9f5
Tripp Whaley <dgw11@duke.edu>
2016-11-21 23:36:46 -0500

    adding sample serialization files in SampleGame folder to be deserialized and tested

--- src/engine/controller/ApplicationController.java
+++ src/engine/controller/ApplicationController.java
@@ -1,8 +1,16 @@
 package engine.controller;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import authoring.model.EnemyData;
+import authoring.model.PlayerData;
+import authoring.model.ProjectileData;
 import authoring.model.TowerData;
+import authoring.model.WeaponData;
 import authoring.model.map.MapData;
 import authoring.model.map.TerrainData;
 import authoring.model.serialization.DeserializeJSON;
@@ -37,21 +45,77 @@ public class ApplicationController {
 
 		SerializeJSON ser = new SerializeJSON();
 		
-		MapData md = new MapData();
-		TerrainData terrain1 = new TerrainData("grass", 0, 0);
-		TerrainData terrain2 = new TerrainData("grass", 0, 1);
-		TerrainData terrain3 = new TerrainData("grass", 1, 0);
-		TerrainData terrain4 = new TerrainData("grass", 1, 1);
 		try {
+			
+			// Terrain Data 
+			TerrainData terrain1 = new TerrainData("grass", 0, 0);
+			TerrainData terrain2 = new TerrainData("water", 0, 1);
+			// Tower Data
+			TowerData tow = new TowerData();
+			tow.setBuyPrice(40);
+			tow.setCollisionRadius(2);
+			tow.setImagePath("src/resources/boss.png");
+			List<Map<Integer, Integer>> list = new ArrayList<Map<Integer, Integer>>();
+			Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+			map.put(100, 10);
+			map.put(20, 50);
+			list.add(map);
+			tow.setInitialLocations(list);
+			tow.setMaxHealth(1000);
+			tow.setMovementStrategy("fast as fuck");
+			tow.setName("tower 1");
+			tow.setSellPrice(100);
+			
+			// Enemy Data
+			EnemyData ed = new EnemyData();
+			ed.setCollisionRadius(5);
+			ed.setImagePath("src/resources/boss.png");
+			ed.setKillReward(100);
+			ed.setMaxHealth(100);
+			ed.setName("enemy 1");
+			ed.setSpeed(10);
+			ed.setWeaponName("weapon 1");
+			
+			// Weapon Data
+			WeaponData wd = new WeaponData();
+			wd.setFireRate(10);
+			wd.setName("weapon 1");
+			wd.setProjectileName("projectile 1");
+			wd.setRange(100);
+			
+			// Projectile Data
+			ProjectileData pd = new ProjectileData();
+			pd.setAreaOfEffectRadius(10);
+			pd.setDamageStrategy("damage");
+			pd.setImagePath("src/resources/boss.png");
+			pd.setMaxRange(10);
+			pd.setMovementStrategy("fast as fuck");
+			pd.setName("projectile 1");
+			
+			// Player Data
+			PlayerData pdd = new PlayerData();
+			pdd.setLoseStrategyName("lose strategy 1");
+			pdd.setStartingCash(2000);
+			pdd.setWinStrategyName("win strategy 1");
+			pdd.setStartingLives(5);
+			
+			// Map Data
+			MapData md = new MapData();
 			md.setNumXCells(2);
 			md.setNumYCells(2);
 			md.addTerrainData(terrain1);
 			md.addTerrainData(terrain2);
-			md.addTerrainData(terrain3);
-			md.addTerrainData(terrain4);
 			md.addSpawnPoint(new Point(0,0));
 			md.addSinkPoint(new Point(1,1));
-			ser.SerializeToFile(md, "SampleMap");
+			
+			ser.SerializeToFile(md, "SampleGame/"+md.getClass().getSimpleName());
+			ser.SerializeToFile(terrain1,"SampleGame/"+terrain1.getClass().getSimpleName()+"1");
+			ser.SerializeToFile(terrain2, "SampleGame/"+terrain2.getClass().getSimpleName()+"2");
+			ser.SerializeToFile(tow, "SampleGame/"+tow.getClass().getSimpleName());
+			ser.SerializeToFile(ed, "SampleGame/"+ed.getClass().getSimpleName());
+			ser.SerializeToFile(wd, "SampleGame/"+wd.getClass().getSimpleName());
+			ser.SerializeToFile(pd, "SampleGame/"+pd.getClass().getSimpleName());
+			ser.SerializeToFile(pdd, "SampleGame/"+pdd.getClass().getSimpleName());
 			
 			
 			TerrainMap terrainMap = new TerrainMap(md);
