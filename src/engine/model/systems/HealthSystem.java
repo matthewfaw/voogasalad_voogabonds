30a230167981f1237151142eec4bb2a818e50c01
Alan Guo <ajg51@duke.edu>
2016-12-05 14:12:41 -0500

    Added health system.

---
+++ src/engine/model/systems/HealthSystem.java
@@ -0,0 +1,13 @@
+package engine.model.systems;
+
+import java.util.List;
+
+import engine.model.components.HealthComponent;
+
+public class HealthSystem {
+	private List<HealthComponent> myHealthComponents;
+	
+	public void changeHealth(CollidableComponent moved, CollidableComponent unmoved){
+		
+	}
+}
