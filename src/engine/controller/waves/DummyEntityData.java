fba67905594ac6f52987779ba8bbb81d8595f2f3
Owen <tc153@duke.edu>
2016-12-06 15:53:58 -0500

    added dummy classes

---
+++ src/engine/controller/waves/DummyEntityData.java
@@ -0,0 +1,14 @@
+package engine.controller.waves;
+
+import authoring.model.IReadableData;
+
+//TODO: change to use the object made by GameData
+public class DummyEntityData implements IReadableData{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
