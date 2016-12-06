277422d2b5a9814ef15f89c6727d2ca6cfc3ce76
Alan Guo <ajg51@duke.edu>
2016-12-06 13:07:01 -0500

    Added movement system.

---
+++ src/engine/model/systems/MovementSystem.java
@@ -0,0 +1,44 @@
+package engine.model.systems;
+
+import java.util.List;
+
+import engine.IObservable;
+import engine.IObserver;
+import engine.controller.timeline.TimelineController;
+import engine.model.components.MoveableComponent;
+
+public class MovementSystem implements IObservable<MovementSystem>, IObserver<TimelineController>, ISystem{
+	private List<IObserver<MovementSystem>> myObservers;
+	private List<MoveableComponent> myMoveableComponents;
+	
+	private void updateNextMoves() {
+		// get next move for all registered movable components
+		// update new position for all registered movable components
+		for (MoveableComponent mc: myMoveableComponents) {
+			// mc.setPosition(mc.getNextMove());
+		}
+	}
+	
+	/********* Observable interface ***********/
+	@Override
+	public void update(TimelineController aChangedObject) {
+		updateNextMoves();
+	}
+
+	/********* Observer interface ***********/
+	@Override
+	public void attach(IObserver<MovementSystem> aObserver) {
+		myObservers.add(aObserver);
+	}
+
+	@Override
+	public void detach(IObserver<MovementSystem> aObserver) {
+		myObservers.remove(aObserver);
+	}
+
+	@Override
+	public void notifyObservers() {
+		myObservers.forEach(o -> o.update(this));
+	}
+
+}
