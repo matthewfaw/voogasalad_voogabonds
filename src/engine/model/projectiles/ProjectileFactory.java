cfb5e0837b4922ed60ecf0d8005f7c6e35b53d87
Weston Carvalho <wac12@duke.edu>
2016-12-10 16:11:52 -0500

    Deprecated projectiles.

--- src/engine/model/projectiles/ProjectileFactory.java
+++ src/engine/model/projectiles/ProjectileFactory.java
@@ -14,6 +14,7 @@ import utility.ResouceAccess;
  * @author Weston
  *
  */
+@Deprecated
 public class ProjectileFactory {
 	private static final String NO_SUCH_PROJECTILE = "NoSuchProjectile";
 	private DataStore<ProjectileData> myProjectiles;
