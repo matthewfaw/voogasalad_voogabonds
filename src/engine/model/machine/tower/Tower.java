99eb410d4ce8df71914afaaa16cdf2010d067b5a
Weston Carvalho <wac12@duke.edu>
2016-12-10 16:06:21 -0500

    Deprecated all Machine files

--- src/engine/model/machine/tower/Tower.java
+++ src/engine/model/machine/tower/Tower.java
@@ -1,14 +1,5 @@
 package engine.model.machine.tower;
 
-import authoring.model.TowerData;
-import engine.controller.timeline.TimelineController;
-import engine.model.components.CollidableComponent;
-import engine.model.machine.Machine;
-import engine.model.playerinfo.IModifiablePlayer;
-import engine.model.weapons.WeaponFactory;
-import javafx.util.Pair;
-import utility.Point;
-
 @Deprecated
 public class Tower{ /*extends Machine implements IUpgradable {
 	
