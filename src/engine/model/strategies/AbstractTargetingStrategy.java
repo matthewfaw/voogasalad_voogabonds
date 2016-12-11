3acfe7f5f4ccd1ed20b1b5bc6c1bdcd569674b81
Weston Carvalho <wac12@duke.edu>
2016-12-10 19:26:26 -0500

    Add within range method to physical system

--- src/engine/model/strategies/AbstractTargetingStrategy.java
+++ src/engine/model/strategies/AbstractTargetingStrategy.java
@@ -5,12 +5,13 @@ import java.util.List;
 import engine.model.components.ITargeting;
 import engine.model.components.PhysicalComponent;
 import engine.model.game_environment.MapMediator;
+import engine.model.systems.PhysicalSystem;
 
 abstract public class AbstractTargetingStrategy implements ITargetingStrategy{
 	
-	protected List<PhysicalComponent> getTargets(MapMediator map, IPhysical location, ITargeting targeter) {
+	protected List<PhysicalComponent> getTargets(PhysicalSystem map, IPhysical location, ITargeting targeter) {
 		//TODO: Allow the strategy to search in wedges instead of circles
-		List<PhysicalComponent> targets = map.withinRange(location.getPosition(), location.getHeading());
+		List<PhysicalComponent> targets = map.withinRange(location.getPosition(), location.getHeading(), targeter.getTargetWedgeRadius(), targeter.getTargetWedgeWidth());
 		
 		return targets;
 	}
--- src/engine/model/strategies/target/BadTargetingStrategy.java
+++ src/engine/model/strategies/target/BadTargetingStrategy.java
@@ -2,16 +2,16 @@ package engine.model.strategies.target;
 
 import engine.model.components.PhysicalComponent;
 import engine.model.components.TargetingComponent;
-import engine.model.game_environment.MapMediator;
 import engine.model.strategies.AbstractTargetingStrategy;
 import engine.model.strategies.IPhysical;
+import engine.model.systems.PhysicalSystem;
 import engine.model.systems.TeamSystem;
 
 
 public class BadTargetingStrategy extends AbstractTargetingStrategy {
 
 	@Override
-	public IPhysical target(MapMediator map, TeamSystem teams, IPhysical location, TargetingComponent targeter) {
+	public IPhysical target(PhysicalSystem map, TeamSystem teams, IPhysical location, TargetingComponent targeter) {
 		IPhysical result = null;
 		for (PhysicalComponent target: getTargets(map, location, targeter)) {
 			if (
