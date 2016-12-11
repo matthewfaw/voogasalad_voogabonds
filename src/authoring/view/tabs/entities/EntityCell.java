1d5155d7936005c35e9f87f0a6bf063c25016200
Niklas Sjoquist <nhs4@duke.edu>
2016-12-10 22:55:47 -0500

    Added delete method for entity delete button

--- src/authoring/view/tabs/entities/EntityCell.java
+++ src/authoring/view/tabs/entities/EntityCell.java
@@ -1,10 +1,13 @@
 package authoring.view.tabs.entities;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 
 /**
  * @author Niklas Sjoquist
@@ -18,4 +21,24 @@ public class EntityCell extends ListCell<String> {
     private Label label = new Label("(empty)");
     private Pane cellPane = new Pane();
     private Button deleteButton = new Button("X");
+    
+    public EntityCell(EntityTab parent) {
+        super();
+        cellBox.getChildren().addAll(label,cellPane,deleteButton);
+        HBox.setHgrow(cellPane, Priority.ALWAYS);
+        deleteButton.setOnAction(deleteEntity(parent));
+    }
+    
+    private EventHandler<ActionEvent> deleteEntity(EntityTab parent) {
+        return new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle (ActionEvent event) {
+                // TODO Auto-generated method stub
+                
+            }
+            
+        };
+    }
+    
 }
