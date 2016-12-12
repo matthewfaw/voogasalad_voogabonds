eb54cc878652a7255ce86035ef8aeae6b2b8e704
Owen <tc153@duke.edu>
2016-12-11 23:26:52 -0500

    created constructors for the components

--- src/engine/model/components/concrete/TargetingComponent.java
+++ src/engine/model/components/concrete/TargetingComponent.java
@@ -1,5 +1,6 @@
 package engine.model.components.concrete;
 
+import authoring.model.ComponentData;
 import engine.model.components.AbstractComponent;
 import engine.model.components.ITargeting;
 import engine.model.strategies.IPosition;
@@ -17,9 +18,12 @@ public class TargetingComponent extends AbstractComponent implements ITargeting 
 	private PhysicalSystem myPhysical;
 	private TeamSystem myTeams;
 	
-	public TargetingComponent(PhysicalSystem physical, TeamSystem teams) {
+	public TargetingComponent(PhysicalSystem physical, TeamSystem teams, ComponentData componentData) {
 		myPhysical = physical;
 		myTeams = teams;
+		mySightRange = Double.parseDouble(componentData.getFields().get("mySightRange"));
+		mySightWidth = Double.parseDouble(componentData.getFields().get("mySightWidth"));
+		myTargetsEnemies = Boolean.parseBoolean(componentData.getFields().get("myTargestEnemies"));
 	}
 	
 	@Override
