f83af0fc31a2828676b826b060b2775b2b29237c
Owen <tc153@duke.edu>
2016-12-14 10:28:35 -0500

    added buttons

--- src/gamePlayerView/GUIFactory/ButtonFactory.java
+++ src/gamePlayerView/GUIFactory/ButtonFactory.java
@@ -3,7 +3,11 @@ package gamePlayerView.GUIFactory;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-
+/**
+ * 
+ * @author owenchung
+ *
+ */
 public class ButtonFactory{
 
 
